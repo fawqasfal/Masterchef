f867bde515d3f7b7921f971dd52a5ad61d6ce929
albert <blahoink@gmail.com>
2014-02-15 13:55:02 -0500

    added main project and desktop project makes black window

---
+++ masterchef-desktop/src/com/masterchef/DesktopGame.java
@@ -0,0 +1,9 @@
+package com.masterchef;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+
+public class DesktopGame {
+	public static void main(String args[]) {
+		new LwjglApplication(new Game(), "Masterchef", 800, 600, false);
+	}
+}

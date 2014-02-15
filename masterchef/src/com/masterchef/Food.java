bb70068d6933a928df911ae0997062d09188a16a
albert <blahoink@gmail.com>
2014-02-15 15:06:41 -0500

    cleese can move now

---
+++ masterchef/src/com/masterchef/Food.java
@@ -0,0 +1,15 @@
+package com.masterchef;
+
+import com.badlogic.gdx.math.Rectangle;
+
+public class Food extends Rectangle {
+	
+	public final int RAW = 0;
+	public final int COOKED = 1;
+	public final int BURNT = 2;
+	public final int ROTTEN = 3;
+	
+	public Food() {
+		super();
+	}
+}
--- masterchef/src/com/masterchef/Game.java
+++ masterchef/src/com/masterchef/Game.java
@@ -1,7 +1,11 @@
 package com.masterchef;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
@@ -17,6 +21,8 @@ public class Game implements ApplicationListener {
 	OrthographicCamera camera;
 	SpriteBatch batch;
 	
+	List<Rectangle> food = new ArrayList<Rectangle>();
+	
 	@Override
 	public void create() {
 		
@@ -26,7 +32,7 @@ public class Game implements ApplicationListener {
 		
 		batch = new SpriteBatch();
 		
-		
+		// just for testing
 		cleese = new Texture(Gdx.files.internal("/root/git/Masterchef/masterchef/src/assets/cleese.png"));
 		cleeseHead = new Rectangle();
 		cleeseHead.x = 0;
@@ -41,14 +47,28 @@ public class Game implements ApplicationListener {
 		// TODO Auto-generated method stub
 
 	}
-
+	public void update() {
+		// horizontal movement
+		if(Gdx.input.isKeyPressed(Input.Keys.A)) {
+			cleeseHead.x--;
+		} else if (Gdx.input.isKeyPressed(Input.Keys.D)) {
+			cleeseHead.x++;
+		}
+		
+	}
 	@Override
 	public void render() {
+		// run update code
+		update();
+		
+		// clear screen
 		Gdx.gl.glClearColor(0, 0, 0.2f, 1);
 		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
 		
+		// update camera
 		camera.update();
 		
+		// draw sprites
 		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
 		batch.draw(cleese, cleeseHead.x, cleeseHead.y);

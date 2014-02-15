c276f107ef9ddd1902babbd2bb581bd673b2deb5
albert <blahoink@gmail.com>
2014-02-15 14:53:42 -0500

    added cleese

--- masterchef-desktop/src/com/masterchef/DesktopGame.java
+++ masterchef-desktop/src/com/masterchef/DesktopGame.java
@@ -4,6 +4,6 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 
 public class DesktopGame {
 	public static void main(String args[]) {
-		new LwjglApplication(new Game(), "Masterchef", 800, 600, false);
+		new LwjglApplication(new Game(), "Masterchef", 800, 800, false);
 	}
 }
--- masterchef/src/com/masterchef/Game.java
+++ masterchef/src/com/masterchef/Game.java
@@ -1,13 +1,39 @@
 package com.masterchef;
 
 import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL10;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
 
 public class Game implements ApplicationListener {
 
+	
+	Texture cleese;
+	Rectangle cleeseHead;
+	
+	OrthographicCamera camera;
+	SpriteBatch batch;
+	
 	@Override
 	public void create() {
-		// TODO Auto-generated method stub
-
+		
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, 400, 400);
+		
+		
+		batch = new SpriteBatch();
+		
+		
+		cleese = new Texture(Gdx.files.internal("/root/git/Masterchef/masterchef/src/assets/cleese.png"));
+		cleeseHead = new Rectangle();
+		cleeseHead.x = 0;
+		cleeseHead.y = 0;
+		cleeseHead.width = 128;
+		cleeseHead.height = 128;
+		
 	}
 
 	@Override
@@ -18,8 +44,16 @@ public class Game implements ApplicationListener {
 
 	@Override
 	public void render() {
-		// TODO Auto-generated method stub
-
+		Gdx.gl.glClearColor(0, 0, 0.2f, 1);
+		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+		
+		camera.update();
+		
+		batch.setProjectionMatrix(camera.combined);
+		batch.begin();
+		batch.draw(cleese, cleeseHead.x, cleeseHead.y);
+		batch.end();
+		
 	}
 
 	@Override
